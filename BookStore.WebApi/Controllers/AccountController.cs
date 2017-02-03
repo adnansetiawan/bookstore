ef6330041a727f1c241aa3468fdff2bbb8dd1fd5
adnan <ase@voxteneo.asia>
2017-02-03 17:09:11 +0700

    add api login

--- BookStore.WebApi/Controllers/AccountController.cs
+++ BookStore.WebApi/Controllers/AccountController.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace BookStore.WebApi.Controllers
@@ -19,20 +21,56 @@ namespace BookStore.WebApi.Controllers
         {
             _userManager = userManager;
         }
-      
-        [Route("Register")]
-        public async Task<IHttpActionResult> Register(UserRequestInput user)
+        [Route("Login")]
+        public async Task<HttpResponseMessage> LoginUser(UserRequestInput user)
         {
-            
+            var request = HttpContext.Current.Request;
+            var tokenServiceUrl = request.Url.GetLeftPart(UriPartial.Authority) + request.ApplicationPath + "/Token";
+            using (var client = new HttpClient())
+            {
+                var requestParams = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", user.UserName),
+                new KeyValuePair<string, string>("password", user.Password)
+            };
+                var requestParamsFormUrlEncoded = new FormUrlEncodedContent(requestParams);
+                var tokenServiceResponse = await client.PostAsync(tokenServiceUrl, requestParamsFormUrlEncoded);
+                var responseString = await tokenServiceResponse.Content.ReadAsStringAsync();
+                var responseCode = tokenServiceResponse.StatusCode;
+                var responseMsg = new HttpResponseMessage(responseCode)
+                {
+                    Content = new StringContent(responseString, Encoding.UTF8, "application/json")
+                };
+                return responseMsg;
+            }
+        }
+        [Route("Register")]
+        public async Task<HttpResponseMessage> Register(UserRequestInput user)
+        {
 
-            IdentityResult result = await _userManager.RegisterUser(new Entities.Inputs.User.UserInput
+
+            IdentityResult identityResult = await _userManager.RegisterUser(new Entities.Inputs.User.UserInput
             {
                 UserName = user.UserName,
                 Password = user.Password
             });
 
-            
-            return Ok();
+            if (!identityResult.Succeeded)
+            {
+                //return BadRequest();
+            }
+
+            // Auto login after registrаtion (successful user registration should return access_token)
+            var loginResult = await this.LoginUser(new UserRequestInput
+            {
+                UserName = user.UserName,
+                Password = user.Password
+            });
+            return loginResult;
+
         }
     }
+
+       
 }
